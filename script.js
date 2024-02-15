3994f3a8ab2821425b5a5891256b63e6386d2aea
ipsanleonardo <159036521+ipsanleonardo@users.noreply.github.com>
2024-02-15 18:36:26 +0800

    script.js

--- script.js
+++ script.js
@@ -1,33 +1,36 @@
-let celsiusInput = document.querySelector('#celsius > input')
-let fahrenheitInput = document.querySelector('#fahrenheit > input')
+// Wait for the DOM content to be fully loaded
+document.addEventListener('DOMContentLoaded', function() {
+    // Select input elements
+    let celsiusInput = document.querySelector('#celsius > input');
+    let fahrenheitInput = document.querySelector('#fahrenheit > input');
 
-let btn = document.querySelector('.button button')
+    // Select button element
+    let btn = document.querySelector('.button button');
 
+    // Function to round a number to two decimal places
+    function roundNumber(number) {
+        return Math.round(number * 100) / 100;
+    }
 
-function roundNumber(number){
-    return Math.round(number*100)/100
-}
+    // Event listener for Celsius input
+    celsiusInput.addEventListener('input', function() {
+        let cTemp = parseFloat(celsiusInput.value);
+        let fTemp = (cTemp * (9 / 5)) + 32;
 
+        fahrenheitInput.value = roundNumber(fTemp);
+    });
 
-/* Celcius to Fahrenheit */
-celsiusInput.addEventListener('input', function(){
-    let cTemp = parseFloat(celsiusInput.value)
-    let fTemp = (cTemp*(9/5)) + 32
+    // Event listener for Fahrenheit input
+    fahrenheitInput.addEventListener('input', function() {
+        let fTemp = parseFloat(fahrenheitInput.value);
+        let cTemp = (fTemp - 32) * (5 / 9);
 
-    fahrenheitInput.value = roundNumber(fTemp)
-})
+        celsiusInput.value = roundNumber(cTemp);
+    });
 
-
-/* Fahrenheit to Celcius */
-fahrenheitInput.addEventListener('input', function(){
-    let fTemp = parseFloat(fahrenheitInput.value)
-    let cTemp = (fTemp - 32) * (5/9)
-
-    celsiusInput.value = roundNumber(cTemp)
-})
-
-
-btn.addEventListener('click', ()=>{
-    celsiusInput.value = ""
-    fahrenheitInput.value = ""
-})
+    // Event listener for button click to clear inputs
+    btn.addEventListener('click', function() {
+        celsiusInput.value = "";
+        fahrenheitInput.value = "";
+    });
+});
